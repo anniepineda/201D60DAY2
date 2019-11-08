8223d9fb48caa1a2c98926864b8fd302f108b767
anniepineda <anniepineda@outlook.com>
2019-11-07 16:22:09 -0800

    partner lab

--- js/app.js
+++ js/app.js
@@ -5,45 +5,62 @@ var nameCapture = prompt('Whats your name?');
 alert('Welcome to my site ' + nameCapture);
 document.getElementById('thefeet').innerHTML = 'Welcome to my site ' + nameCapture + '.';
 
+function myEducation () {
 var higherEducation = prompt('Do I have higher education?');
 if (higherEducation.toUpperCase() === 'YES'){
     alert('You are correct!');
 }else{
     alert('Wrong! I do!');
 }
+}
+myEducation();
 //console.log('Higher Education:', higherEducation);
 
+function mySeattle () {
 var seattleNative = prompt('Am I originally from Seattle?');
 if (seattleNative.toUpperCase() === 'NO'){
     alert('You are correct!');
 }else{
     alert('Wrong! I am not!');
 }
+}
+mySeattle();
 //console.log('Seattle Native:', seattleNative);
 
+function myMilitary () {
 var militaryService = prompt('Have I served in the military?');
 if (militaryService.toUpperCase() === 'YES'){
     alert('You are correct!');
 }else{
     alert('Wrong! I did!');
 }
+}
+myMilitary();
 //console.log('Military Service:', militaryService);
 
+function myAmazon () {
 var previousAmazonian = prompt('Have I previously worked at Amazon?');
 if (previousAmazonian.toUpperCase() === 'YES'){
     alert('You are correct!');
 }else{
     alert('Wrong! I am a transfer!');
 }
+}
+myAmaznn();
 //console.log('Previous Amazonian:', previousAmazonian);
 
+function myExperience () {
 var jsExperience = prompt('Do I have any previous JavaScript experience?');
 if (jsExperience.toUpperCase() === 'YES'){
     alert('You are correct!');
 }else{
     alert('Wrong! I do!');
 }
+}
+myExperience();
 
+
+function myNumbers () {
 var number;
 var counter = 1;
 
@@ -64,10 +81,14 @@ while(number !== 39){
         alert('Too many guesses...');
         break;
     }
-    //alert('Correct! I am 39!')
 }
+}
+myNumbers();
+    //alert('Correct! I am 39!')
+
 //console.log('Prev js experience:', jsExperience);
 
+function myCities () {
 var cities = ['madrid', 'miami', 'paris', 'berlin', 'seattle', 'washington'];
 var answer = prompt('Name a city I have visited!');
 var lastCounter = 1;
@@ -89,6 +110,8 @@ while(lastCounter < 6){
         }
     }
 }
+}
+myCities();
    //break;
 
 
